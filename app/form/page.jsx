c2b7f801a946e735eddbef0e122f33ebdceb7839
K-Tran2001 <khoatran135.246@gmail.com>
2023-11-03 10:17:14 +0700

    8th commit

--- app/form/page.jsx
+++ app/form/page.jsx
@@ -14,7 +14,7 @@ export default function Form() {
 
     const request = {
         "source_lang": "en",
-        "target_lang":JSON.parse(localStorage.getItem('lang'))?.code||"vi",
+        "target_lang":JSON.parse(localStorage.getItem('prefered_local'))?.code||"vi",
         "click_to_upload":"Click to upload",
         "or_drag_and_drop":"or drag and drop",
         "take_a_photo":"Take A Photo",
